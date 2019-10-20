7734c7b3d9b8bef0602e19079f900b7fa70bd9a8
Deni678 <32527466+Deni678@users.noreply.github.com>
2019-10-19 21:26:14 -0700

    Update meeting2.R

--- weeks_1_and_2/meeting2.R
+++ weeks_1_and_2/meeting2.R
@@ -1,19 +1,32 @@
 5+3
 number<-10
 num
+# Vectors
 times<-c(10,5,"NA")
 mean(times)
+mean(times, na.rm = TRUE)
+
+mean(na.rm = TRUE, x = times)
+
 range(times)
 sqrt(times)
 times[times <30]
 which(times<10)
 times[which(times<10)]
 times<- c(10,5,4,20,50)
+times > 4 & times < 20
+times < 20 | times > 10
+sum(times < 10)
+all(times < 10)
+
+# Subset
 times[3]
 times[-3]
 times[c(2,4)]
 times[c(4,2)]
 times[c(2:5)]
+
+
 mtcars
 head(mtcars)
 tail(mtcars)
@@ -22,11 +35,14 @@ ncol(mtcars)
 colnames(mtcars)[1]
 mtcars$wt
 colnames(mtcars)[which("wt") ]
+# == is used for comparison ; = is used for assignment statements
 mtcars[colnames(mtcars)=="wt"]
 summary(mtcars)
 which(colnames(mtcars)=="wt")
 mtcars$cyl
 mean(mtcars$mpg)
+
+# Packages
 install.packages("tibble")
 library("tibble")
 ?tibble
